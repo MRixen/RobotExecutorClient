61ff23c46fd8365f9a19e3f6dbcc57a6936f6856
MRixen <ManLeo.Rixen@gmail.com>
2016-08-08 08:20:52 +0200

    Some minor changes

--- robotExecutorClient.ino
+++ robotExecutorClient.ino
@@ -9,6 +9,12 @@ union DXL_IDs
 	byte bytes[2];
 };
 
+union DXL_MSG_START
+{
+	short dxl_msg_start;
+	byte bytes[4];
+};
+
 union DXL_POSITION_1
 {
 	short dxl_position;
@@ -61,48 +67,56 @@ void usbInterrupt(byte* buffer, byte nCount) {
 
 	if (debugMode) toggleLED();
 
+	// Read everything from stream
 	for (unsigned int i = 0; i < nCount; i++) {
-		//if (debugMode && (i == 1) || (i == 0)) Serial1.print((char)i);
-		if (debugMode ) Serial1.print(buffer[i]);
-		//Serial1.print(buffer[i]);
+		if (debugMode) Serial1.write(buffer[i]);
 		controlData[i] = buffer[i];
 	}
 
-	// Extract the ids
-	DXL_IDs ids;
-	ids.bytes[0] = controlData[1];
-	ids.bytes[1] = controlData[0];
+	// Extract start message from stream
+	DXL_MSG_START msgStart;
+	msgStart.bytes[0] = controlData[0];
+	msgStart.bytes[1] = controlData[1];
+	msgStart.bytes[2] = controlData[2];
+	msgStart.bytes[3] = controlData[3];
 
+	if (debugMode) Serial1.write(buffer[i]);
+	// Extract ids from stream
+	//DXL_IDs ids;
+	//ids.bytes[0] = controlData[2];
+	//ids.bytes[1] = controlData[3];
 
-	for (size_t i = 0; i < 16; i++) {
+	// TODO Print the ids to see what is inside
+
+	//for (size_t i = 0; i < 16; i++) {
 
 		//idData[i] = ids.dxl_ids & (1 << i);
 		//Serial1.println(idData[i]);
 		//Serial1.println((1 << i));
-	}
+	//}
 
 	// Extract speed
-	dxl_speed = controlData[2];
+	//dxl_speed = controlData[4];
 
 	// Extract positions
-	DXL_POSITION_1 position_1;
-	position_1.bytes[0] = controlData[3];
-	position_1.bytes[1] = controlData[4];
+	/*DXL_POSITION_1 position_1;
+	position_1.bytes[0] = controlData[5];
+	position_1.bytes[1] = controlData[6];
 
 	DXL_POSITION_2 position_2;
-	position_2.bytes[0] = controlData[5];
-	position_2.bytes[1] = controlData[6];
+	position_2.bytes[0] = controlData[7];
+	position_2.bytes[1] = controlData[8];
 
 	positionData[0] = position_1.dxl_position;
-	positionData[1] = position_2.dxl_position;
+	positionData[1] = position_2.dxl_position;*/
 
+	// TODO: Add SPEED data
+	// TODO: Print the last data
 	for (size_t i = 0; i < 2; i++) {
-		//if (idData[i] != 0) Dxl.writeWord(idData[i], DXL_COMMANDS.GOAL_POSITION, positionData[i]);
-		//if (debugMode) Serial1.print("idData: ");
-		//if (debugMode) Serial1.println(idData[i]);
+		if (idData[i] != 0) Dxl.writeWord(idData[i], DXL_COMMANDS.GOAL_POSITION, positionData[i]);
+		//if (debugMode) Serial1.write(idData[i]);
 
-		//if (debugMode) Serial1.print("positionData: ");
-		//if (debugMode) Serial1.println(positionData[i]);
+		//if (debugMode) Serial1.write(positionData[i]);
 	}
 }
 
